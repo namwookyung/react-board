72aae642febea664c87f623aeffd57f7a8a519f0
namwookyung <namwookyung.dev@gmail.com>
2022-09-19 18:13:27 +0900

    22.09.19

--- client/src/components/PostAdd.js
+++ client/src/components/PostAdd.js
@@ -1,57 +1,80 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./PostAdd.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import Axios from "axios";
 
-class PostAdd extends Component {
-  render() {
-    return (
-      <div className="postAdd">
-        <div className="full_layer">
-          <div className="addContent">
-            <div>
-              <h3>게시글 등록</h3>
-            </div>
-            <div>
-              <CKEditor
-                editor={ClassicEditor}
-                data="<p>내용을 입력해주세요.</p>"
-                onReady={(editor) => {
-                  console.log("Editor is ready to use!", editor);
-                }}
-                onChange={(event, editor) => {
-                  const data = editor.getData();
-                  console.log({ event, editor, data });
-                }}
-                onBlur={(event, editor) => {
-                  console.log("Blur.", editor);
-                }}
-                onFocus={(event, editor) => {
-                  console.log("Focus.", editor);
-                }}
-              />
-            </div>
-            <div className="btnLine">
-              <button
-                type="submit"
-                class="closeBtn"
-                onClick={this.props.onClose}
-              >
-                등록
-              </button>
-              <button
-                type="button"
-                className="closeBtn"
-                onClick={this.props.onClose}
-              >
-                닫기
-              </button>
-            </div>
+function PostAdd() {
+  const [addContent, setAddContent] = useState({
+    postIdx: "",
+    postTitle: "",
+    postContent: "",
+    postDate: "",
+  });
+
+  const submitPost = () => {
+    Axios.post("/api/insert", {
+      postTitle: addContent.postTitle,
+      postContent: addContent.postTitle,
+    }).then(() => {
+      alert("등록 완료");
+    });
+  };
+
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    setAddContent({
+      ...addContent,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className="postAdd">
+      <div className="full_layer">
+        <div className="addContent">
+          <div className="content">
+            <h3>게시글 등록</h3>
+            <input
+              type="text"
+              className="postInput"
+              placeholder="제목"
+              onChange={getValue}
+              name="postTitle"
+            />
+            <CKEditor
+              editor={ClassicEditor}
+              data="<p>내용을 입력해주세요.</p>"
+              className="contentEditor"
+              onReady={(editor) => {
+                console.log("Editor is ready to use!", editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />
+            <button type="submit" className="closeBtn" onClick={submitPost}>
+              등록
+            </button>
+            {/* <button
+              type="button"
+              className="closeBtn"
+              onClick={this.props.onClose}
+            >
+              닫기
+            </button> */}
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default PostAdd;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 const PORT = process.env.port || 8000;
 
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "nwk951207@",
-  database: "board",
+  database: "post",
 });
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const postTitle = req.body.postTitle;
+  const postContent = req.body.postContent;
   const sqlQuery =
-    "INSERT INTO post (postIdx, postTitle, postContent, postDate) VALUES ('1', '테스트 제목', '테스트 내용', '2022-09-16')";
-  db.query(sqlQuery, (err, result) => {
+    "INSERT INTO post (postTitle, postContent) VALUES ('제목', '내용?')";
+  db.query(sqlQuery, [postTitle, postContent], (err, result) => {
     res.send("-----success-----");
   });
 });
